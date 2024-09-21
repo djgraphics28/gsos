e2fcc8ac4873c4a3955082a7e680da0cd3d3b7c6
darwin ibay <darwin.i@shoredigitalinc.com>
2024-09-21 08:03:04 +0800

    updates

--- resources/views/livewire/roles-and-permissions/edit.blade.php
+++ resources/views/livewire/roles-and-permissions/edit.blade.php
@@ -2,7 +2,7 @@
     @section('title', 'Edit Role')
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 class="h3 mb-0 text-gray-800">{{ __('Edit Role') }}</h1>
-        <a wire:navigate href="{{ route('roles.permissions') }}" class="d-none d-sm-inline-block btn btn-sm btn-secondary shadow-sm">
+        <a href="{{ route('roles.permissions') }}" class="d-none d-sm-inline-block btn btn-sm btn-secondary shadow-sm">
             <i class="fas fa-arrow-left fa-sm text-white-50"></i> Back
         </a>
     </div>
